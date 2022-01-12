14158edba14f33d90a59a5b945fe1b687b19ed52
angelacuahutle <angela.cuahutle@gmail.com>
2022-01-12 16:49:08 -0600

    Add calculator file and string from file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import Calculator from './Components/calculator';
 import 'bootstrap/dist/css/bootstrap.css';
 
-const App = () => <div><h1>Lorem</h1></div>;
+const App = () => <div><h1>Calculator</h1></div>;
 export default App;
---
+++ src/Components/calculator.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+}
+
+render () {
+  return (
+    <div>Wellcome To Calculator</div>
+  );
+ }
+
+ export default Calculator;
