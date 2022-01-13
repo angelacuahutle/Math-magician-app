3443a22c62c948fb66492fc906b24e8f53395791
angelacuahutle <angela.cuahutle@gmail.com>
2022-01-12 18:15:13 -0600

    Add Class components

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from 'react';
 import Calculator from './Components/calculator';
 import 'bootstrap/dist/css/bootstrap.css';
 
-const App = () => <div className="container my-4"><Calculator /></div>;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Calculator />
+      </div>
+    );
+  }
+}
+
 export default App;
