68e5f6bb7e3dba723ead83be9cc5c2f46623ec76
angelacuahutle <angela.cuahutle@gmail.com>
2022-01-14 19:15:12 -0600

    Add stateobject to operation and fix some fix to calculator

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -10,13 +10,14 @@ const CalculatorHook = () => {
     setStateObj(result);
   };
 
-  const { next, total } = this.state;
   return (
 
     <div className="calculator card">
 
       <div type="text" className="calculator-screen z-depth-1 " placeholder="0" disabled>
-        {next ?? total ?? 0}
+      {stateObj.total}
+        {stateObj.operation}
+        {stateObj.next}
       </div>
       <div className="calculator-keys">
 
@@ -48,4 +49,4 @@ const CalculatorHook = () => {
     </div>
   );
 }
-export default Calculator;
+export default CalculatorHook;
